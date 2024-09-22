1b21edcedf0d904c9014f6743ec62c5a13e9f9b5
Zade <lukaslightie7@gmail.com>
2024-09-22 15:33:37 +0300

    Init commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export default function app() {
+    return (
+        <main className="w-screen h-screen grid place-items-center bg-zinc-800">
+            <h1 className="text-8xl uppercase font-black text-blue-400">Hello world!</h1>
+        </main>
+    )
+}
