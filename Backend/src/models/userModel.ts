189715740a8c74bb7645be83cdb2909e478ec2ee
DanVolu <dan.voluj@gmail.com>
2024-09-26 11:52:39 +0300

    User fetch requests created

--- Backend/src/models/userModel.ts
+++ Backend/src/models/userModel.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import { cartItemSchema, cartInterface } from "./cartModel";
 
 const userSchema = new mongoose.Schema({
-    _id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
+    id: {
+        type: Number,
+        required: true,
+        unique: true
     },
     first_name: {
         type: String,
@@ -21,6 +23,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
     admin: {
         type: Boolean,
         default: false
@@ -34,17 +40,21 @@ const userSchema = new mongoose.Schema({
         default: 0
     },
     cart: [
-        {
-            productId: {
-                type: mongoose.Schema.Types.ObjectId,
-                required: true,
-            },
-            quantity: {
-                type: Number,
-                default: 1
-            }
-        }
+        cartItemSchema
     ]
 });
 
-export default mongoose.model("User", userSchema)
+export default mongoose.model("User", userSchema);
+
+export interface UserInterface {
+    _id: number,
+    first_name: string,
+    last_name: string,
+    email: string,
+    password: string,
+    isVerified: boolean,
+    admin: boolean,
+    created_at: Date,
+    wallet?: number,
+    cart?: cartInterface
+}
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,9 +1,26 @@
 import express from "express";
 
 import StartDB from "./Database/MongoDB";
+import dotenv from "dotenv";
+import routes from "./routes/index";
 
+
+dotenv.config();
+
+
+const PORT = process.env.PORT || 4040;
 const app = express();
+app.use(express.json());
+app.use("/api/v1", routes);
 
-app.listen(3000, () => console.log("[express] Server is running on port 3000"));
+app.listen(4040, () => console.log(`[express] Server is running on port http://localhost:${PORT}`));
+
+app.get('/status', (req, res) => {
+    res.status(200).json({
+      status: "success",
+      message: "Jeigu servas veikia tai ko tu neveiki"
+    });
+});
+  
 
 StartDB();
