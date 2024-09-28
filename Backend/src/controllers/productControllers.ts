8630eacb428c2985349a735378ab3971b8d5af94
Zade <lukaslightie7@gmail.com>
2024-09-28 22:16:58 +0300

    tiny fixes

--- Backend/src/controllers/productControllers.ts
+++ Backend/src/controllers/productControllers.ts
@@ -1,14 +1,6 @@
-import express, { Request, Response } from "express";
+import { Request, Response } from "express";
 import Product from "../models/productModel";
 import { generateProductId } from "../utils/idgen";
-
-interface ProductInterface {
-  id: Number | string;
-  name: string;
-  description: string;
-  price: number;
-  image: string;
-}
 
 const  productControllers = {
   createProduct: async (req: Request, res: Response) => {
@@ -23,9 +15,13 @@ const  productControllers = {
         image,
       });
       const savedProduct = await newProduct.save();
-      res.status(201).json({ message: "Success", savedProduct }); // SUBJECT TO CHANGE
+      res.status(201).json({ 
+        message: "Success", savedProduct 
+      }); // SUBJECT TO CHANGE
     } catch (error) {
-      res.status(400).json({ message: "Failure", error }); // SUBJECT TO CHANGE
+      res.status(400).json({ 
+        message: "Failure", error 
+      }); // SUBJECT TO CHANGE
     }
   },
   getAllProducts: async (req: Request, res: Response) => {
@@ -33,15 +29,24 @@ const  productControllers = {
       const databaseRes = await Product.find({});
       res.json({ message: "Success", databaseRes }); // SUBJECT TO CHANGE
     } catch (error) {
-      res.send({ message: "Failure", error }); // SUBJECT TO CHANGE
+      res.send({ 
+        message: "Failure", 
+        error 
+      }); // SUBJECT TO CHANGE
     }
   },
   getProductById: async (req: Request, res: Response) => {
     try {
       const databaseRes = await Product.findOne({ id: req.body.id });
-      res.send({ message: "Success", databaseRes }); // SUBJECT TO CHANGE
+      res.send({ 
+        message: "Success", 
+        databaseRes 
+      }); // SUBJECT TO CHANGE
     } catch (error) {
-      res.send({ message: "Failure", error }); // SUBJECT TO CHANGE
+      res.send({ 
+        message: "Failure", 
+        error 
+      }); // SUBJECT TO CHANGE
     }
   },
   patchProduct: async (req: Request, res: Response) => {
@@ -49,7 +54,6 @@ const  productControllers = {
 
     Object.keys(req.body).forEach((key) => {
       if (req.body[key] !== undefined && req.body[key] !== null) {
-        // Technicly useless, subject to change
         fieldsToUpdate[key] = req.body[key];
       }
     });
