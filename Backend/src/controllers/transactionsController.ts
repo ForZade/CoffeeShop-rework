774ed79b644b5dd6e8462fdb565ee2d187c1096f
Zade <lukaslightie7@gmail.com>
2024-10-01 01:01:29 +0300

    quick fixes

--- Backend/src/controllers/transactionsController.ts
+++ Backend/src/controllers/transactionsController.ts
@@ -1,75 +1,76 @@
 import { Request, Response, NextFunction } from "express";
-import User, { UserInterface } from "../models/userModel"
+import User, { UserInterface } from "../models/userModel";
 import Transaction, { TransactionInterface } from "../models/transactionModel";
 import { verifyToken, TokenInterface } from "../utils/token";
 import generateTransactionId from "../utils/idgen";
 import { fakeTransaction } from "../utils/fakeTransaction";
-import transactionModel from "../models/transactionModel";
+// import transactionModel from "../models/transactionModel";
 
 const transactionsController = {
-    makeTransaction: async (req: Request, res: Response, next: NextFunction) => {
-        const authHeader = req.headers.authorization;
-        const token = authHeader && authHeader.split(" ")[1];
+  makeTransaction: async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.jwt;
 
-        try {
-            const decoded: TokenInterface = await verifyToken(token!);
+    try {
+      const decoded: TokenInterface = await verifyToken(token);
 
-            const user: UserInterface = await User.findOne({ id: decoded.id });
-            const id = generateTransactionId();
+      const user: UserInterface = await User.findOne({ id: decoded.id });
+      const id = generateTransactionId();
 
-            if (!user) {
-                return res.status(404).json({
-                    message: "User not found",
-                })
-                }
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
 
-            const newTransaction: TransactionInterface = new Transaction({
-                id,
-                user_id: user.id,
-                order_details: user.cart,
-            });
+      const newTransaction: TransactionInterface = new Transaction({
+        id,
+        user_id: user.id,
+        order_details: user.cart,
+      });
 
-            if (await fakeTransaction(newTransaction, req.body.cardData)) {  /* Reikia dar padaryt kad darant pirkima paimam kortos duomenis*/
-                user.cart = [];
-                user.save();
-                newTransaction.save();
+      if (await fakeTransaction(newTransaction, req.body.cardData)) {
+        /* Reikia dar padaryt kad darant pirkima paimam kortos duomenis*/
+        user.cart = [];
+        user.save();
+        newTransaction.save();
 
-                return res.status(201).json({
-                    message: "Transaction successful",
-                    newTransaction,
-                })
-            }
-            else {
-                return res.status(400).json({
-                    message: "Transaction failed",
-                    reason: "Insuffiecient funds",
-                })
-            }
-        } catch (err) {
-            next(err);
-        }
-    },
-    getTransactions: async (req: Request, res: Response, next: NextFunction) => {
-        const authHeader = req.headers.authorization;
-        const token = authHeader && authHeader.split(" ")[1];
-        try {
-            const decoded: TokenInterface = await verifyToken(token!);
-            const transaction: TransactionInterface = await Transaction.findOne({ id: decoded.id });
-            
-            if (!transaction) {
-                return res.status(404).json({
-                    message: "No purchases made",
-                })
-            };
-
-            res.status(200).json({
-                message: "Transactions retrieved successfully",
-                transactions: transaction
-            });
-        } catch (err) {
-            next(err);
-        };
+        return res.status(201).json({
+          message: "Transaction successful",
+          newTransaction,
+        });
+      } else {
+        return res.status(400).json({
+          message: "Transaction failed",
+          reason: "Insuffiecient funds",
+        });
+      }
+    } catch (err) {
+      next(err);
     }
-}
+  },
+  getTransactions: async (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(" ")[1];
+    try {
+      const decoded: TokenInterface = await verifyToken(token!);
+      const transaction: TransactionInterface = await Transaction.findOne({
+        id: decoded.id,
+      });
 
-export default transactionsController
+      if (!transaction) {
+        return res.status(404).json({
+          message: "No purchases made",
+        });
+      }
+
+      res.status(200).json({
+        message: "Transactions retrieved successfully",
+        transactions: transaction,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+};
+
+export default transactionsController;
--- Backend/src/middlewares/cardValidator.ts
+++ Backend/src/middlewares/cardValidator.ts
@@ -1,25 +1,29 @@
-import { body } from 'express-validator';
+import { body } from "express-validator";
 
 export const CARD_VALIDATOR = [
-    body("cardNumber")
-        .isLength({ min: 13, max: 19 })
-        .withMessage('CARD NUMBER MUST BE BETWEEN 13 AND 19 CHARACTERS'),
-    body("cvc")
-        .isLength({ min: 3, max: 4 })
-        .withMessage('CVC/CVV 3 DIGITS, CID 4 DIGITS'),
-    body('expiryDate')
-        .custom((value) => { // Example used : "05/27"
-            const today = new Date();
-            const currentMonth = today.getMonth() + 1;
-            const currentYear = today.getFullYear() % 1000; // 2024 / 1000 Liekana 0024 -> 24
-            const month = parseInt(value.slice(0, 2), 10); // "05" -> 5
-            const year = parseInt(value.slice(2, 4), 10);  // "27" -> 27     
+  body("cardNumber")
+    .isLength({ min: 13, max: 19 })
+    .withMessage("CARD NUMBER MUST BE BETWEEN 13 AND 19 CHARACTERS"),
+  body("cvc")
+    .isLength({ min: 3, max: 4 })
+    .withMessage("CVC/CVV 3 DIGITS, CID 4 DIGITS"),
+  body("expiryDate")
+    .custom((value) => {
+      // Example used : "05/27"
+      const today = new Date();
+      const currentMonth = today.getMonth() + 1;
+      const currentYear = today.getFullYear() % 1000; // 2024 / 1000 Liekana 0024 -> 24
+      const month = parseInt(value.slice(0, 2), 10); // "05" -> 5
+      const year = parseInt(value.slice(2, 4), 10); // "27" -> 27
 
-            // 27   > 24              24   === 24             06    >= 05
-            if(year > currentYear || (year === currentYear && month >= currentMonth)) {
-                return false
-            }
-            return true;
-        })
-        .withMessage("EXPIRED CARD")
+      // 27   > 24              24   === 24             06    >= 05
+      if (
+        year > currentYear ||
+        (year === currentYear && month >= currentMonth)
+      ) {
+        return false;
+      }
+      return true;
+    })
+    .withMessage("EXPIRED CARD"),
 ];
