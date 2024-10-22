3521da857e921fe73effbe6d0736fd2098a64da2
Zade <lukaslightie7@gmail.com>
2024-10-22 20:12:12 +0300

    minor fixes before merge

--- Frontend/src/pages/ContactsPage.tsx
+++ Frontend/src/pages/ContactsPage.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
 import ContactsForm from "../components/Forms/ContactsForm";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function ContactsPage () {
+    const { checkAuth } = useAuth();
+
+    useEffect(() => {
+        const loadPage = async () => {
+            try {
+                await checkAuth();
+            }
+            catch (error) {
+                console.error('Error:', error);
+            }
+        }
+
+        loadPage();
+    })
+
 
     return(
         <ContactsForm/>
