7b440fd274f4da9f998466f0a2c6a9e6cb05fd6b
xvqpz <adomaskaminski@gmail.com>
2024-11-07 20:25:39 +0200

    refresh on add done/contacts form only for logged in users done

--- Frontend/src/pages/ProductPage.tsx
+++ Frontend/src/pages/ProductPage.tsx
@@ -106,27 +106,31 @@ export default function ProductPage() {
             {!loading && <img src="/jacobs.webp" alt={product.name} className="object-contain max-h-[700px]"/>}
           </aside>
 
-          <article className="w-full h-full col-span-3 px-8 py-6 flex flex-col justify-between relative">
-            <header className="w-full flex flex-col gap-4">
-              <h3 className="text-xl font-bold tracking-widest">{product.category}</h3>
-              <h1 className="text-5xl font-black">{product.name}</h1>
-              <p className="text-xl">{product.description}</p>
-            </header>
+                  <article className="w-full h-full col-span-3 px-8 py-6 flex flex-col justify-between relative">
+          <header className="w-full flex flex-col gap-4">
+            <h3 className="text-xl font-bold tracking-widest truncate overflow-hidden whitespace-nowrap">
+              {product.category}
+            </h3>
+            <h1 className="text-5xl font-black truncate overflow-hidden whitespace-nowrap">
+              {product.name}
+            </h1>
+            <p className="text-xl">{product.description}</p>
+          </header>
 
-            <div className="flex flex-col gap-6">
-              <h3 className="text-2xl font-bold">Packaging:</h3>
-              <footer className="w-full flex items-center justify-between">
-                <div className="bg-gradient-to-br from-coffee-200 to-coffee-100 px-6 py-2 rounded-md font-bold text-xl">
-                  {product.size}
-                </div>
-                <h3 className="text-3xl font-black">
-                  {product.price.$numberDecimal}€
-                </h3>
-              </footer>
+          <div className="flex flex-col gap-6">
+            <h3 className="text-2xl font-bold">Packaging:</h3>
+            <footer className="w-full flex items-center justify-between">
+              <div className="bg-gradient-to-br from-coffee-200 to-coffee-100 px-6 py-2 rounded-md font-bold text-xl">
+                {product.size}
+              </div>
+              <h3 className="text-3xl font-black">
+                {product.price.$numberDecimal}€
+              </h3>
+            </footer>
 
-              {auth && <AddToCart productId={product.id} />}
-            </div>
-          </article>
+            {auth && <AddToCart productId={product.id} />}
+          </div>
+        </article>
 
           {auth && <ReviewButton liked={product.liked} productId={product.id} extraClass="absolute top-6 right-12 scale-150"/>}
 
